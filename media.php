cc8354e86f8e854c663373f2d51e3aa8bf3fa58c
rizalvalry <rizal.muh.rzl@gmail.com>
2021-12-27 20:02:02 +0700

    favicon

--- media.php
+++ media.php
@@ -30,8 +30,15 @@ $tahun = date("Y");
         <meta name="robots" content="index, follow" />
         
         <!-- Favicon -->
-		<link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
+		<link rel="shortcut icon" type="image/x-icon" href="favicon_io_ddsm/favicon.ico">
         <!-- individu source -->
+        <link rel="apple-touch-icon" sizes="180x180" href="favicon_io_ddsm/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon_io_ddsm/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon_io_ddsm/favicon-16x16.png">
+        <link rel="manifest" href="favicon_io_ddsm/site.webmanifest">
+        <link rel="mask-icon" href="favicon_io_ddsm/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
 	
 
 
